4584c64efe84bbd9affedc6d2844bdf32c00886d
Anderson Hung <anderson.hung@live.com>
2016-10-19 11:29:02 -0400

    Fixed

--- spec/features/add_to_carts_spec.rb
+++ spec/features/add_to_carts_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
+RSpec.feature "AddToCarts", type: :feature do
 
   # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -18,13 +18,15 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
   end
 
   scenario "They see all products" do
-    # ACT
-    visit root_path
+  # ACT
+  visit root_path
 
-    # DEBUG
-    save_screenshot
+  click_on 'Add'
 
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
+  # DEBUG
+  sleep 1
+  save_screenshot
+
+  expect(page).to have_text 'My Cart (1)'
   end
 end
--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,12 +1,11 @@
 require 'rails_helper'
 
-RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
-
-  # SETUP
+RSpec.feature "ProductDetails", type: :feature, js: true do
+    # SETUP
   before :each do
     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
+    1.times do |n|
       @category.products.create!(
         name:  Faker::Hipster.sentence(3),
         description: Faker::Hipster.paragraph(4),
@@ -16,15 +15,14 @@ RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
       )
     end
   end
-
-  scenario "They see all products" do
+  scenario "They go to product detail page" do
     # ACT
     visit root_path
 
+    click_on 'Details'
+    expect(page).to have_css '.products-show'
     # DEBUG
     save_screenshot
 
-    # VERIFY
-    expect(page).to have_css 'article.product', count: 10
   end
 end
